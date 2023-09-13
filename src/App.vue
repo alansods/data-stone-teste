06fd2368fbac0f25eb569a796f0ddf433baaf407
Alan Santos <alansods@gmail.com>
2023-09-13 19:36:58 -0300

    criacao navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app>
+    <NavBar/>
     <v-main>
+      <transition mode="out-in">
       <router-view />
+    </transition>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import NavBar from "@/layouts/NavBar.vue";
+
 </script>
--- src/layouts/NavBar.vue
+++ src/layouts/NavBar.vue
@@ -5,19 +5,16 @@
       @click.stop="drawer = !drawer"
     ></v-app-bar-nav-icon>
 
-    <v-toolbar-title>My files</v-toolbar-title>
+    <v-toolbar-title>Sistema de Cadastro</v-toolbar-title>
 
-    <v-spacer></v-spacer>
-
-    <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-    <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
   </v-app-bar>
 
-  <v-navigation-drawer v-model="drawer" location="bottom" temporary>
-    <v-list :items="items"></v-list>
+  <v-navigation-drawer v-model="drawer" location="left" temporary>
+    <v-list>
+      <v-list-item link to="/pagina1">Início</v-list-item>
+      <v-list-item link to="/pagina2">Lista de Clientes</v-list-item>
+      <v-list-item link to="/pagina3">Lista de Produtos</v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
