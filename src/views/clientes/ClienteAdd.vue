1b215d53c7639f3cadbd29773af257a3d2425be4
Alan Santos <alansods@gmail.com>
2023-09-14 19:05:19 -0300

    regras de validação do formulario adicionar cliente

--- src/views/clientes/ClienteAdd.vue
+++ src/views/clientes/ClienteAdd.vue
@@ -1,68 +1,75 @@
 <template>
   <div>
     <div class="text-h5 font-weight-bold mb-10">Adicionar Cliente</div>
-    <v-row dense>
-      <v-col cols="12">
-        <v-text-field
-          v-model="novoCliente.nome"
-          label="Nome"
-          required
-          :rules="nomeRules"
-        ></v-text-field>
-      </v-col>
-    </v-row>
+    <v-form @submit.prevent="adicionarCliente">
+      <v-row dense>
+        <v-col cols="12">
+          <v-text-field
+            v-model="novoCliente.nome"
+            label="Nome"
+            required
+            :rules="nomeRules"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-    <v-row dense>
-      <v-col cols="12">
-        <v-text-field
-          v-model="novoCliente.email"
-          label="E-mail"
-          required
-          type="email"
-          :rules="emailRules"
-        ></v-text-field>
-      </v-col>
-    </v-row>
+      <v-row dense>
+        <v-col cols="12">
+          <v-text-field
+            v-model="novoCliente.email"
+            label="E-mail"
+            required
+            type="email"
+            :rules="emailRules"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-    <v-row dense>
-      <v-col cols="12">
-        <v-text-field
-          v-model="novoCliente.telefone"
-          label="Telefone"
-          required
-          :rules="telefoneRules"
-          v-maska:[options]
-        ></v-text-field>
-      </v-col>
-    </v-row>
+      <v-row dense>
+        <v-col cols="12">
+          <v-text-field
+            v-model="novoCliente.telefone"
+            label="Telefone"
+            required
+            :rules="telefoneRules"
+            v-maska:[options]
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-    <v-row dense>
-      <v-col cols="auto">
-        <v-checkbox v-model="novoCliente.ativo" label="Ativo"></v-checkbox>
-      </v-col>
-    </v-row>
+      <v-row dense>
+        <v-col cols="auto">
+          <v-checkbox v-model="novoCliente.ativo" label="Ativo"></v-checkbox>
+        </v-col>
+      </v-row>
 
-    <v-row dense>
-      <v-col cols="12" sm="auto"
-        ><v-btn type="submit" color="primary" @click="adicionarCliente">Adicionar</v-btn></v-col
-      >
-      <v-col cols="12" sm="auto"
-        ><v-btn variant="text" color="primary" @click="$router.push('/')"
-          >Voltar</v-btn
-        ></v-col
-      >
-    </v-row>
+      <v-row dense>
+        <v-col cols="12" sm="auto"
+          ><v-btn
+            type="submit"
+            color="primary"
+            :disabled="hasErrors"
+            >Adicionar</v-btn
+          ></v-col
+        >
+        <v-col cols="12" sm="auto"
+          ><v-btn variant="text" color="primary" @click="$router.push('/')"
+            >Voltar</v-btn
+          ></v-col
+        >
+      </v-row>
+    </v-form>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useAppStore } from "@/store/app";
 import { Cliente } from "@/types/appTypes";
 
-import { vMaska } from "maska"
+import { vMaska } from "maska";
 
-const options = { mask: '(##) # ####-####' };
+const options = { mask: "(##) # ####-####" };
 
 const novoCliente = ref<Cliente>({
   id: Math.random(),
@@ -75,31 +82,44 @@ const novoCliente = ref<Cliente>({
 
 const appStore = useAppStore();
 
-const adicionarCliente = () => {
-    appStore.clientes.push(novoCliente.value);
+const adicionarCliente = (): void => {
+  console.log("FOI");
+  appStore.clientes.push(novoCliente.value);
 
-    novoCliente.value = {
-      id: 0,
-      nome: "",
-      documento: "",
-      telefone: "",
-      email: "",
-      ativo: false,
-    };
+  novoCliente.value = {
+    id: 0,
+    nome: "",
+    documento: "",
+    telefone: "",
+    email: "",
+    ativo: false,
+  };
 };
 
 const nomeRules = [
-  (v: string) => !!v || "O nome é obrigatório",
+  (v: string) => !!v || "Obrigatório",
   (v: string) =>
     (v && v.length <= 50) || "O nome deve ter no máximo 50 caracteres",
 ];
 
 const emailRules = [
-  (v: string) => !!v || "O e-mail é obrigatório",
+  (v: string) => !!v || "Obrigatório",
   (v: string) => /.+@.+\..+/.test(v) || "E-mail inválido",
 ];
 
-const telefoneRules = [
-  (v: string) => !!v || "O telefone é obrigatório",
-];
+const telefoneRules = [(v: string) => (v && v.length > 15) || "Obrigatório"];
+
+const hasErrors = computed(() => {
+  const nomeRulesPassed = nomeRules.every(
+    (rule) => rule(novoCliente.value.nome) === true
+  );
+  const emailRulesPassed = emailRules.every(
+    (rule) => rule(novoCliente.value.email) === true
+  );
+  const telefoneRulesPassed = telefoneRules.every(
+    (rule) => rule(novoCliente.value.telefone) === true
+  );
+
+  return !(nomeRulesPassed && emailRulesPassed && telefoneRulesPassed);
+});
 </script>
